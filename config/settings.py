bfa345715778a9909d7d66ff9672214dc0b5fd10
unknown <edd.edwardmike@gmail.com>
2021-09-10 01:58:50 +0100

    config for AWS access

--- config/settings.py
+++ config/settings.py
@@ -262,18 +262,19 @@ django_heroku.settings(locals())
 # AWS config
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = 'AKIAS2UHTDCKUVNREGYS'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-AWS_QUERYSTRING_AUTH = False
 AWS_STORAGE_BUCKET_NAME = 'ukvite'
 
-# AWS_AUTO_CREATE_BUCKET = True
+AWS_QUERYSTRING_AUTH = False
 
-# MEDIA_URL = f"https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/"
+AWS_AUTO_CREATE_BUCKET = True
 
-# _AWS_EXPIRY = 60 * 60 * 24 * 7
+MEDIA_URL = f"https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/"
+
+_AWS_EXPIRY = 60 * 60 * 24 * 7
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-# AWS_S3_OBJECT_PARAMETERS = {
-#     "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
-# }
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
+}
